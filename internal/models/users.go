eb9e5cb17cbbf2cfe6346e850e1b070377fb3d06
Daria Fominykh <dashka.fominykh@gmail.com>
2023-04-02 15:46:01 +0300

    Add a user login

--- internal/models/users.go
+++ internal/models/users.go
@@ -44,8 +44,30 @@ VALUES(?, ?, ?, UTC_TIMESTAMP())`
 }
 
 // Authenticate verifies whether a user exists with the provided email address and password. Returns the relevant user ID if they do.
-func (m *UserModel) Authenticate(name, email, password string) (int, error) {
-	return 0, nil
+func (m *UserModel) Authenticate(email, password string) (int, error) {
+	var id int
+	var hashedPassword []byte
+
+	stmt := "SELECT id, hashed_password FROM users WHERE email = ?"
+
+	err := m.DB.QueryRow(stmt, email).Scan(&id, &hashedPassword)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return 0, ErrInvalidCredentials
+		} else {
+			return 0, err
+		}
+	}
+
+	err = bcrypt.CompareHashAndPassword(hashedPassword, []byte(password))
+	if err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+			return 0, ErrInvalidCredentials
+		} else {
+			return 0, err
+		}
+	}
+	return id, nil
 }
 
 // Exists checks if a user exists with a specific ID.
